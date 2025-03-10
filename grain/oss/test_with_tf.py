bf821bbe65e52c56f36feeb4e1e330de5177f9ba
Ihor Indyk <ihor.indyk@gmail.com>
2025-03-10 11:56:18 -0700

    Update test_with_tf.py

--- grain/oss/test_with_tf.py
+++ grain/oss/test_with_tf.py
@@ -1,6 +1,16 @@
+import importlib
+
+original_import = importlib.import_module
+def _import_module(name, package=None):
+  print(f"iindyk importing: {name}")
+  return original_import(name, package)
+
+importlib.import_module=original_import
+
 import abc
 import typing
 import base64
+import re
 
 import tensorflow as tf
 from grain import python as grain
