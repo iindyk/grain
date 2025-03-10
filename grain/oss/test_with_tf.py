60565f18ea42dc62313fcd6971593d3a5f2ac1fe
Ihor Indyk <ihor.indyk@gmail.com>
2025-03-10 11:32:21 -0700

    Update test_with_tf.py

--- grain/oss/test_with_tf.py
+++ grain/oss/test_with_tf.py
@@ -1,5 +1,5 @@
-from hanging_threads import start_monitoring
-start_monitoring(seconds_frozen=10, test_interval=100)
+import abc
+import typing
 
 import tensorflow as tf
 from grain import python as grain
