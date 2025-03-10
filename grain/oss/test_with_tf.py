16df773c6b41fc3029379fb5d86e1f50664df805
Ihor Indyk <ihor.indyk@gmail.com>
2025-03-10 12:18:23 -0700

    Update test_with_tf.py

--- grain/oss/test_with_tf.py
+++ grain/oss/test_with_tf.py
@@ -1,24 +1,24 @@
-import importlib
-print("importlib", flush=True)
+# import importlib
+# print("importlib", flush=True)
 
-original_import = importlib.__import__
-def _import_module(*args, **kwargs):
-  print(f"iindyk importing: {args[0]}", flush=True)
-  return original_import(*args, **kwargs)
+# original_import = importlib.__import__
+# def _import_module(*args, **kwargs):
+#   print(f"iindyk importing: {args[0]}", flush=True)
+#   return original_import(*args, **kwargs)
 
-importlib.__import__=original_import
+# importlib.__import__=original_import
 
-import abc
-print("abc", flush=True)
-import typing
-print("typing", flush=True)
-import base64
-print("base64", flush=True)
-import re
-print("re", flush=True)
+# import abc
+# print("abc", flush=True)
+# import typing
+# print("typing", flush=True)
+# import base64
+# print("base64", flush=True)
+# import re
+# print("re", flush=True)
 
 import tensorflow as tf
-print("tf", flush=True)
+# print("tf", flush=True)
 from grain import python as grain
 print("grain", flush=True)
 print(f"iindyk: Done {grain.MapDataset}", flush=True)
