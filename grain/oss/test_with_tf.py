6d20e4304d748d6bbe9530338078ff22e57ecfc1
Ihor Indyk <ihor.indyk@gmail.com>
2025-03-10 12:08:12 -0700

    Update test_with_tf.py

--- grain/oss/test_with_tf.py
+++ grain/oss/test_with_tf.py
@@ -1,24 +1,24 @@
 import importlib
-print("importlib")
+print("importlib", flush=True)
 
-original_import = importlib.import_module
-def _import_module(name, package=None):
-  print(f"iindyk importing: {name}")
-  return original_import(name, package)
+original_import = importlib.__import__
+def _import_module(*args, **kwargs):
+  print(f"iindyk importing: {args[0]}", flush=True)
+  return original_import(*args, **kwargs)
 
-importlib.import_module=original_import
+importlib.__import__=original_import
 
 import abc
-print("abc")
+print("abc", flush=True)
 import typing
-print("typing")
+print("typing", flush=True)
 import base64
-print("base64")
+print("base64", flush=True)
 import re
-print("re")
+print("re", flush=True)
 
 import tensorflow as tf
-print("re")
+print("tf", flush=True)
 from grain import python as grain
-print("grain")
-print(f"iindyk: Done {grain.MapDataset}")
+print("grain", flush=True)
+print(f"iindyk: Done {grain.MapDataset}", flush=True)
