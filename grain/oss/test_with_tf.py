ac9098812eb97854c66222377eb867aa834b55bc
Ihor Indyk <ihor.indyk@gmail.com>
2025-03-12 09:35:14 -0700

    Update test_with_tf.py

--- grain/oss/test_with_tf.py
+++ grain/oss/test_with_tf.py
@@ -21,7 +21,7 @@
 #gc.disable()
 import tensorflow as tf
 print(f"iindyk: imported TF", flush=True)
-from array_record.python import array_record_module
+from grain import python as grain
 
 # print("tf", flush=True)
 print(f"iindyk: Done {tf.Tensor}", flush=True)
